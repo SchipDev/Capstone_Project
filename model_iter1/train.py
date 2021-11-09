478d77de6cd1fcd53d294a660bfd13d6ac0b00dd
SchipDev <shane99@vt.edu>
2021-11-09 14:54:43 -0500

    updating dataLoader and training script

--- model_iter1/train.py
+++ model_iter1/train.py
@@ -1,41 +1,54 @@
-
-   
 from model import *
 import pandas as pd
 import numpy as np
-from matplotlib import image
+from matplotlib import image, interactive
 from matplotlib import pyplot
 from dataLoader import *
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.preprocessing.image import array_to_img
 from tensorflow.keras.preprocessing.image import save_img
-#import tensorflow.keras.utils.image_dataset_from_directory
+import os
+import shutil as sh
+import matplotlib.pyplot as plt
 
+#os environ gpu
 # Load Training/Testing Data
-native_chips, _05masks = load_data("/projects/cmda_capstone_2021_ti/data/training_sets/trainingset_descending_40.csv")
-#print(len(native_chips))
-# im = load_img("/projects/cmda_capstone_2021_ti/data/Data/NativeChips/Chip_Native_B200_OR_ABI-L2-CMIPC-M3C13_G16_s20171912142189_e20171912144574_c20171912145010_EP552995097.411295.png", color_mode = "grayscale")
-# img_array = img_to_array(im)
+# native_chips, _05masks = load_data("/projects/cmda_capstone_2021_ti/data/training_sets/trainingset_descending_40.csv")
 
-#ds = dataset_from_directory("/projects/cmda_capstone_project_ti/data/training_sets/NativeChips", image_size=(400,400))
-#print(ds)
+data_gen_args = dict(rotation_range=0,
+                    width_shift_range=0,
+                    height_shift_range=0,
+                    shear_range=0,
+                    zoom_range=0,
+                    horizontal_flip=True,
+                    fill_mode='nearest')
+myGene = trainGenerator(2,'/projects/cmda_capstone_2021_ti/data/training_sets','NativeChips','05masks',data_gen_args,save_to_dir = None)
+print(myGene)
 # Construct model
 model = unet()
 
 
 # Fit Model
-#preds = model.predict(img_array.reshape([-1,400, 400,1]))
-#asdfg = array_to_img(preds[0])
-#save_img("model_IO_test_output.png", preds[0])
-print(model.fit(native_chips, _05masks, epochs=25))
 
-#preds = model.predict(img_array.reshape([-1,400, 400,1]))
-#asdfg = array_to_img(preds[0])
-#save_img("model_IO_test_output.png", preds[0])
+# history = model.fit(native_chips, _05masks, epochs=140)
+model.fit(myGene, epochs=140)
+# os.mkdir("model_iter1/model_accuracy")
+# plt.plot(history.history["accuracy"])
+# # plt.plot(history.history["val_accuracy"])
+# plt.title("Modle Accuracy")
+# plt.ylabel("Accuracy")
+# plt.xlabel("Epoch")
+# #plt.legend(["Train", "Test"], loc='upper_left')
+# plt.savefig("model_iter1/model_accuracy/Accuracy_Plot.png")
 
-preds = model.predict(img_array.reshape([-1,400, 400,1]))
-asdfg = array_to_img(preds[0])
-save_img("model_IO_test_output.png", preds[0])
+# preds = model.predict(native_chips[0].reshape([-1,400, 400,1]))
+# pred_img = preds[0]
+# true_chip = native_chips[0]
+# true_mask = _05masks[0]
+# save_img("model_iter1/model_accuracy/trained_model_sample_pred.png", pred_img.clip(max=1))
+# save_img("model_iter1/model_accuracy/Predicted_native_chip.png", true_chip)
+# save_img("model_iter1/model_accuracy/True_Mask.png", true_mask)
 
-# Save Model
+# # Save Model
+# model.save("model_iter1/saved_model")
